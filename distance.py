5fb373038490e392b56463c0d4a661f4345a3464
Malusk <dementordgr@live.com>
2023-02-23 10:38:59 -0600

    distance optimization
    changed euclidean distance to one line

--- distance.py
+++ distance.py
@@ -1,21 +1,21 @@
 import math
-my_list = []
-with open("output.txt", "r") as f:
-    for line in f:
-        # Strip any trailing newline characters from the line
-        line = line.rstrip()
-        # Split the line into a list using commas as the delimiter
-        values = line.split(",")
-        # Convert each value to its appropriate data type and append to my_list
-        my_list.append([float(v) for v in values])
-distance = []
-print(len(my_list))
-for a in range(len(my_list)):
-    for j in range(a+1,len(my_list)):
-        for i in range(len(my_list[0])):
-            distance.append(my_list[a][i] - my_list[j][i])
-        diffsquare = 0
-        for j in distance:
-            diffsquare += j**2
-        print(math.sqrt(diffsquare))
-        distance.clear()
+def main():
+	my_list = []
+	with open("output.txt", "r") as f:
+		for line in f:
+			# Strip any trailing newline characters from the line
+			line = line.rstrip()
+			# Split the line into a list using commas as the delimiter
+			values = line.split(",")
+			# Convert each value to its appropriate data type and append to my_list
+			my_list.append([float(v) for v in values])
+
+	for i in range(len(my_list)):
+		for j in range(i+1,len(my_list)):
+			print(euclidean_distance(my_list[i],my_list[j]))
+		
+def euclidean_distance(x, y):
+	return math.sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)]))
+	
+if __name__ == "__main__":
+	main()
